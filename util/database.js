9f5ab5c790f780271c1e9501f402f8954f0ae180
muhammed ladeed <81908636+ladeedladi@users.noreply.github.com>
2021-06-13 23:04:22 +0530

    Delete util directory

--- util/database.js
+++
@@ -1,6 +0,0 @@
-const Sequelize=require('sequelize');
-const sequelize = new Sequelize('user-details','root','Ladeed@123',{
-    host:'localhost',
-    dialect:'mysql'
-})
- module.exports=sequelize;
