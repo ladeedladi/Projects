32e9ad0a66b213ba271d83373cea7925c9ff2ecf
*ladeed <muhammed.ladeed123@gmail.com>
2021-08-16 11:47:03 +0530

    font awesome added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
